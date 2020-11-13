1ecb4bbc0cdbef1eccbfcb5792cf93fb83642d46
Kai-Builder <gkai@gmail.com>
2020-11-13 17:13:27 -0500

    upd

---
+++ flour/python/libraryies.py
@@ -0,0 +1,17 @@
+from flour.python.calcium import milk
+class Sweets():
+    Candy = True
+    Pizza = True
+    WarHeads = True
+    Anything_Good = True
+    
+class milk():
+    calcium = True
+    milk = milk
+    if calcium == True:
+        print('You are getting your daily calc!')
+        
+        
+class Flour():
+    def start():
+        print('Using Flour Workspace 1.0.')
