10ea1bc1e3f028a6b7e800591ae8d301bcc4c616
SeymoTheDev <72935852+SeymoTheDev@users.noreply.github.com>
2020-11-13 11:34:36 -0500

    update class.py

--- flour/python/class.py
+++ flour/python/class.py
@@ -1,5 +1,11 @@
+from flour.python.calcium import milk
 class Sweets():
     Candy = True
     Pizza = True
     WarHeads = True
     Anything_Good = True
+    
+class milk():
+    calcium = True
+    milk = milk
+    
