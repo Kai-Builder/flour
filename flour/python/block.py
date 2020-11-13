c6a3b196aff0b440bd15b6e6840fa2eeb0844de2
Kai-Builder <gkai@gmail.com>
2020-11-13 12:25:45 -0500

    Create block.py

---
+++ flour/python/block.py
@@ -0,0 +1,9 @@
+from flour.python.class import milk
+milk.calcium = True
+for True in milk.calcium:
+    a = milk.calcium[True]
+    if a == True:
+        while True and a != False:
+            print('call')
+        else: 
+            break
